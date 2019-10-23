eb3012366755021a5781e46d50b7a4fd453193f0
morelli <marcos.morelli@neoway.com.br>
2019-10-23 06:52:51 -0300

    new editions; prices base update; new cards into collection;

--- src/main/java/mtg/collection/editions/Editions.java
+++ src/main/java/mtg/collection/editions/Editions.java
@@ -8,6 +8,10 @@ public enum Editions {
 	/**
 	 * Expansions
 	 */
+		
+	eld_alternate("5414", "Throne of Eldraine Alternate Frame"),
+		
+	eld("5414", "Throne of Eldraine"),
 	
 	m20("5409", "Core Set 2020"),
 	
@@ -241,6 +245,8 @@ public enum Editions {
 	 * Promo Cards
 	 */
 	
+	pack("0000", "Promo Pack", "Promo Pack"),
+	
 	store("0000", "Store Championship", "Store Championship"),
 	
 	draft("0000", "Draft Weekend", "Draft Weekend"),
@@ -339,6 +345,8 @@ public enum Editions {
 	 * Command Zone Series
 	 */
 	
+	c19("5413", "Commander 2019"),
+	
 	c18("5391", "Commander 2018"),
 	
 	cm2("5386", "Commander Anthology Volume II"),
@@ -368,8 +376,8 @@ public enum Editions {
 	/**
 	 * From The Vault
 	 */
-		
-	//spell_gideon("5411", "Signature Spellbook: Gideon"),
+
+	spell_gideon("5411", "Signature Spellbook: Gideon"),
 	
 	spell_jace("5388", "Signature Spellbook: Jace"),
 
